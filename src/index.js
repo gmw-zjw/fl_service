2a1f8c3bba0d9bf11f79191f0de6612c207a7179
gmw-zjw <mingwei2222@126.com>
2019-05-23 12:45:05 +0800

    分离业务

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const Koa = require('koa')
-const mongoose = require('mongoose')
 const Router = require('koa-router')
 const app = new Koa()
 let router = new Router()
-const { connect, initSchema } = require('./db/init')
 
 let user = require('./router/user')
 router.use('/user', user.routes())
 
 // 链接数据库
-;(async () => {
-  await connect()
-  initSchema()
-  const user = mongoose.model('User')
-  let onceUser = new user({username: '执念12', password: '123'})
+// ;(async () => {
+//   await connect()
+//   initSchema()
+//   const user = mongoose.model('User')
+//   let onceUser = new user({username: '执念12', password: '123'})
 
-  onceUser.save().then(() => {
-    console.log('注册成功')
-  })
+//   onceUser.save().then(() => {
+//     console.log('注册成功')
+//   })
 
-})()
+// })()
 
 app.use(router.routes())
 app.use(router.allowedMethods())
