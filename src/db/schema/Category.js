5628729dafa4819c6ec2a339661f54eb99355249
gmw-zjw <mingwei2222@126.com>
2019-05-23 22:21:11 +0800

    增加category模型

--- src/db/schema/Category.js
+++ src/db/schema/Category.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const categorySchema = new Schema({
-
+  mallCategoryId: String,
+  mallCategoryName: String,
+  bxMallSubDto: [
+    {
+      mallSubId: String,
+      mallCategoryId: String,
+      mallSubName: String,
+      comments: Schema.Types.Mixed
+    }
+  ]
 })
 
 mongoose.model('Categorys', categorySchema)
